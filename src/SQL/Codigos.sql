2bcaff6e4caad8ef5baa6f3da192fd798877c45b
MATEUS HENRIQUE DOS SANTOS <mateus_santos6@estudante.sesisenai.org.br>
2024-11-18 21:33:25 -0300

    Ciação SQL

--- src/SQL/Codigos.sql
+++ src/SQL/Codigos.sql
@@ -1,1 +1,32 @@
-create database 
+CREATE DATABASE SA;
+
+CREATE EXTENSION IF NOT EXISTS "pgcrypto"
+
+CREATE TABLE estoque (
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY, 
+    descricao VARCHAR(30) NOT NULL,
+    quantidade INTEGER NOT NULL,
+    data_entrada DATE NOT NULL,
+    data_saida DATE NOT NULL,
+    custo DECIMAL NOT NULL,
+    status BOOLEAN NOT NULL,
+);
+
+CREATE TABLE producao (
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    id_veiculo UUID NOT NULL,
+    id_estoque UUID NOT NULL,
+    FOREIGN KEY (id_veiculo) REFERENCES veiculo(id) ON DELETE CASCADE,
+    FOREIGN KEY (id_estoque) REFERENCES estoque(id) ON DELETE CASCADE,
+);
+
+CREATE TABLE veiculo (
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY, 
+	motor VARCHAR(30) NOT NULL,
+    chassi VARCHAR UNIQUE NOT NULL,
+    cambio VARCHAR(30) NOT NULL,
+    bancos VARCHAR(30) NOT NULL,
+    rodas VARCHAR(30) NOT NULL,
+    cor VARCHAR(30) NOT NULL,
+);
+
