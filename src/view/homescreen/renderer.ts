e37515b496dda6d362a49ac95876b667ad57fde1
MATEUS HENRIQUE DOS SANTOS <mateus_santos6@estudante.sesisenai.org.br>
2024-11-28 20:58:54 -0300

    1010

--- src/view/homescreen/renderer.ts
+++ src/view/homescreen/renderer.ts
@@ -1,6 +1,6 @@
 import Veiculo from '../../entity/Veiculo';
 import './index.css';
-
+import * as echarts from 'echarts'
 
 
 //-----------------------------------------------------------------------------------------------------------------------------------------------------
@@ -17,3 +17,36 @@ document.getElementById("botao-home")?.addEventListener("click", async(event: Mo
   (window as any).navegacaoAPI.paginaHome();
   console.log("teste 2");
 })
+
+document.getElementById("buscar-amount-by-category").addEventListener("click", async(event: MouseEvent)) => {
+  const values = await (window as any).navegacaoAPI.findByCategory();
+  console.log(values)
+
+  const amount = []
+  const categories = []
+
+for(var i=0; i< values.length; i++){
+  amount.push(values[i].amount)
+  categories.push(values[i].category)
+}
+
+  const div = document.getElementById("grafico") as HTMLDivElement;
+  const grafico = echarts.init(div)
+
+  const option = {
+    xAxis: {
+      type: 'category',
+      data: categories
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: [
+      {
+        data: amount,
+        type: 'bar'
+      }
+    ]
+  }
+  grafico.setOption(option);
+}
